5cf4e1350f58ad63d7ed2b2d7caf84a71909e8a6
Thibaut Rey <thibautrey81@gmail.com>
2023-03-02 17:36:20 +0100

    include eigen

--- cpp_version/compile.sh
+++ cpp_version/compile.sh
@@ -1,1 +1,1 @@
-g++ -o stack_images main.cpp alignement.cpp remove_light_pollution.cpp stack_images.cpp `pkg-config --cflags --libs opencv4` -std=c++11
+g++ -I/usr/include/eigen3 -o stack_images main.cpp alignement.cpp remove_light_pollution.cpp stack_images.cpp `pkg-config --cflags --libs opencv4` -std=c++11
