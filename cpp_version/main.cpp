8403d40196cf3e2fa318a8bc5eddc495ff90550f
Thibaut Rey <thibautrey81@gmail.com>
2023-03-02 17:30:04 +0100

    fix

--- cpp_version/main.cpp
+++ cpp_version/main.cpp
@@ -2,30 +2,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
 #include "alignement.cpp"
 #include "remove_light_pollution.cpp"
 #include "stack_images.cpp"
-#include "draw_constellation.cpp"
-
-#define TFT_CS 1
-#define TFT_DC 0
-#define SPI_SPEED 16000000
+#include "draw_constellations.cpp"
 
 using namespace cv;
 
 int main() {
-    // Initialize wiringPi and SPI
-    wiringPiSetup();
-    wiringPiSPISetup(0, SPI_SPEED);
-
-    // Initialize the TFT screen
-    pinMode(TFT_CS, OUTPUT);
-    pinMode(TFT_DC, OUTPUT);
-    digitalWrite(TFT_CS, HIGH);
-    digitalWrite(TFT_DC, HIGH);
-
     // Open the video capture device
     VideoCapture capture(0);
     if (!capture.isOpened()) {
@@ -36,11 +20,6 @@ int main() {
     // Set the camera resolution to 640x480
     capture.set(CAP_PROP_FRAME_WIDTH, 640);
     capture.set(CAP_PROP_FRAME_HEIGHT, 480);
-
-    // Load the constellation template images
-    std::vector<Mat> constellationTemplates;
-    constellationTemplates.push_back(imread("constellation1.jpg", IMREAD_GRAYSCALE));
-    constellationTemplates.push_back(imread("constellation2.jpg", IMREAD_GRAYSCALE));
 
     // Create a vector to store the input images
     std::vector<Mat> inputImages;
@@ -58,12 +37,12 @@ int main() {
         // Add the frame to the list of input images
         inputImages.push_back(frame);
 
-        // If we have enough input images, stack them and display the result on the TFT screen
+        // If we have enough input images, stack them and display the result on the default display
         if (inputImages.size() >= 10) {
             Mat stackedImage;
             stackImages(inputImages, stackedImage);
 
-            // Convert the stacked image to 16-bit color format for display on the TFT screen
+            // Convert the stacked image to 16-bit color format for display on the default display
             Mat displayImage;
             stackedImage.convertTo(displayImage, CV_16U, 256.0 / 65535.0);
             cvtColor(displayImage, displayImage, COLOR_GRAY2RGB);
@@ -72,11 +51,9 @@ int main() {
             //Size referenceSize(100, 100); // Set the reference size for the constellation templates
             //drawConstellations(displayImage, referenceSize);
 
-            // Send the image data to the TFT screen using SPI
-            digitalWrite(TFT_CS, LOW);
-            digitalWrite(TFT_DC, HIGH);
-            wiringPiSPIDataRW(0, (unsigned char*)displayImage.data, 320 * 240 * 2);
-            digitalWrite(TFT_CS, HIGH);
+            // Show the image on the default display
+            imshow("Stacked Image", displayImage);
+            waitKey(1);
 
             // Clear the input images vector
             inputImages.clear();
