2730eb960a8f81851123751ee178f37d744f6d15
Thibaut Rey <thibautrey81@gmail.com>
2023-03-02 17:49:51 +0100

    fix

--- cpp_version/compile.sh
+++ cpp_version/compile.sh
@@ -1,1 +1,1 @@
-g++ -Wall -Wextra -O3 -I/usr/include/eigen3 -o stack_images main.cpp alignement.cpp remove_light_pollution.cpp stack_images.cpp pkg-config --cflags --libs opencv4 -std=c++11
+g++ -Wall -Wextra -O3 -I/usr/include/eigen3 -o stack_images main.cpp alignement.cpp remove_light_pollution.cpp stack_images.cpp `pkg-config --cflags --libs opencv4` -std=c++11
